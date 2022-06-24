d5528241c40d91b7ec73d3349eb6bd5a12627acf
Zakaria Ybeggazene <zakariaybeggazene@gmail.com>
2022-06-24 23:54:16 +0200

    create question based on step first try

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
+from moviesbot.message_processing import create_question
 
 app = Flask(__name__)
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
 VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
 bot = Bot(ACCESS_TOKEN)
+
+step = 0
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
@@ -20,22 +23,24 @@ def receive_message():
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
         # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        message_text = message['message'].get('text')
+                        response = create_question(step, message_text)
+                        step += 1
+                        #response_sent_text = get_message()
+                        send_message(recipient_id, response)
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
-
 
 def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
---
+++ moviesbot/message_processing.py
@@ -0,0 +1,5 @@
+"""Module to process messages recieved by messegner chatbox
+"""
+
+def create_question(step, message):
+    return f"{step} Hello, I am MoviesBot, I can help you find a good movie according to you mood and likes!"
