4d2e850b583ec0d1e4e5c4a536e7b70467959e7c
Zakaria Ybeggazene <zakariaybeggazene@gmail.com>
2022-06-26 18:22:35 +0200

    fix recommendations and add sklearn requirements

--- moviesbot/query_db.py
+++ moviesbot/query_db.py
@@ -2,41 +2,39 @@
 """
 import pandas as pd
 from difflib import SequenceMatcher
-# #Import TfIdfVectorizer from scikit-learn
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# # Import linear_kernel
-# from sklearn.metrics.pairwise import linear_kernel
+#Import TfIdfVectorizer from scikit-learn
+from sklearn.feature_extraction.text import TfidfVectorizer
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
 
-# #Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
-# tfidf = TfidfVectorizer(stop_words='english')
-# #Replace NaN with an empty string
-# df2['overview'] = df2['overview'].fillna('')
-# #Construct the required TF-IDF matrix by fitting and transforming the data
-# tfidf_matrix = tfidf.fit_transform(df2['overview'])
-# # Compute the cosine similarity matrix
-# cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-# #Construct a reverse map of indices and movie titles
-# indices = pd.Series(df2.index, index=df2['title']).drop_duplicates()
-
+#Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
+#Replace NaN with an empty string
 df = pd.read_csv("data/movies.csv")
+df['overview'] = df['overview'].fillna('')
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(df['overview'])
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+#Construct a reverse map of indices and movie titles
+indices = pd.Series(df.index, index=df['title']).drop_duplicates()
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def get_recommendations(title, cosine_sim=None):#cosine_sim):
     # # Get the index of the movie that matches the title
-    # idx = indices[title]
-    # # Get the pairwsie similarity scores of all movies with that movie
-    # sim_scores = list(enumerate(cosine_sim[idx]))
-    # # Sort the movies based on the similarity scores
-    # sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    # # Get the scores of the 10 most similar movies
-    # sim_scores = sim_scores[1:11]
-    # # Get the movie indices
-    # movie_indices = [i[0] for i in sim_scores]
-    # # Return the top 10 most similar movies
-    # return df['title'].iloc[movie_indices]
-    pass
+    idx = indices[title]
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    # Get the scores of the 3 most similar movies
+    sim_scores = sim_scores[1:4]
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+    # Return the top 10 most similar movies
+    return df[['title','homepage']].iloc[movie_indices]
 
 def get_director_similar(director) :
     max_sim = 0
@@ -66,7 +64,7 @@ def query_db(answer_json):
     answer = answer_json['answer']
     dfn = pd.DataFrame(columns=df.columns)
     if isEmpty(answer):
-        movies_list = str(df.iloc[:3].values.tolist())
+        movies_list = str(df[['title','homepage']].iloc[:3].values.tolist())
         return movies_list
     if (answer['title'] != ''):
         movies_list = str(get_recommendations(answer['title']).values.tolist())
